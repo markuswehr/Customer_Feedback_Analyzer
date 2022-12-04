e570fa6fd78bd748449293b40f378d2748e8edf5
Markus Wehr <wehr.markus@web.de>
2022-12-04 18:50:49 +0100

    removed redundant code

--- src/visualization/visuals.py
+++ src/visualization/visuals.py
@@ -27,28 +27,5 @@ def plot_keywords_per_topic(keywords: list):
     
     fig.update_layout(height=600, width=800, title_text="Topics from App Store")
 
-    # create the figure
-    #fig = px.bar(
-        # specify the data for the bar charts
-    #    keywords_list,
-        # use the 'group' barmode to arrange the bars in columns
-    #    barmode='group',
-    #    subplot_titles=[str(f"Topic {x}") for x in range(1, len(keywords_list))]
-    #)
-
-    # create a figure object
-    #fig = go.Figure()
-
-    # iterate over the list of lists
-    #for topic in keywords_list:
-        # create a bar chart using the current list of values
-    #    bar = go.Bar(x=list(range(1, len(topic) + 1)), y=topic)
-        
-        # add the bar chart to the figure
-    #    fig.add_trace(bar)
-        
-    
-    # create a plotly bar chart that shows the top keywords for each topic
-    #fig = px.bar(keywords_list[0], x=list(range(1, len(keywords_list[0]) + 1)), y=keywords_list[0], orientation="h")
-
     return fig
+    
